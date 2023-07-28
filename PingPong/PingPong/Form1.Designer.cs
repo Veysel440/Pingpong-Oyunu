515ca013b3409ad37ab945f2a1ddd876e3e7938e
Veysel440 <109949012+Veysel440@users.noreply.github.com>
2023-07-28 19:58:11 +0300

    Initial commit

---
+++ PingPong/PingPong/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbSolR = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbSagR = new System.Windows.Forms.PictureBox();
+            this.pbTop = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSolR)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSagR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSolR
+            // 
+            this.pbSolR.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbSolR.Location = new System.Drawing.Point(0, 130);
+            this.pbSolR.Name = "pbSolR";
+            this.pbSolR.Size = new System.Drawing.Size(10, 80);
+            this.pbSolR.TabIndex = 0;
+            this.pbSolR.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(-2, 355);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(552, 88);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BAŞLAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbSagR
+            // 
+            this.pbSagR.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbSagR.Location = new System.Drawing.Point(540, 130);
+            this.pbSagR.Name = "pbSagR";
+            this.pbSagR.Size = new System.Drawing.Size(10, 80);
+            this.pbSagR.TabIndex = 2;
+            this.pbSagR.TabStop = false;
+            // 
+            // pbTop
+            // 
+            this.pbTop.BackColor = System.Drawing.Color.Turquoise;
+            this.pbTop.ImageLocation = "pi.jpg";
+            this.pbTop.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbTop.InitialImage")));
+            this.pbTop.Location = new System.Drawing.Point(240, 150);
+            this.pbTop.Name = "pbTop";
+            this.pbTop.Size = new System.Drawing.Size(20, 20);
+            this.pbTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTop.TabIndex = 3;
+            this.pbTop.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(461, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "left Skor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(457, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "rightıSkor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(517, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(517, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(554, 437);
+            this.Controls.Add(this.pbTop);
+            this.Controls.Add(this.pbSagR);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pbSolR);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSolR)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSagR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTop)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSolR;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbSagR;
+        public System.Windows.Forms.PictureBox pbTop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PingPong/PingPong/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string yatayYon = "sag";
+        string dikeyYon = "ust";
+        int topHiz = 10;
+        int solSkor = 0, sagSkor=0;
+        Random rnd = new Random();
+        int RasgeleSayi;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RasgeleSayi = rnd.Next(2);
+            timer1.Enabled = true;
+            label3.Text = "0";
+            label4.Text = "0";
+            button1.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (dikeyYon=="ust"){
+                pbTop.Top = pbTop.Top - 10;
+            }
+
+            if (dikeyYon == "alt")
+            {
+                pbTop.Top = pbTop.Top + 10;
+            }
+
+
+            if (yatayYon == "sol")
+            {
+                pbTop.Left = pbTop.Left - topHiz;
+            }
+
+            if (yatayYon == "sag")
+            {
+                pbTop.Left = pbTop.Left + topHiz;
+            }
+            
+            if (pbTop.Top <= 0)
+            {
+                dikeyYon = "alt";
+            }
+
+            if (pbTop.Top >= 340)
+            {
+                dikeyYon = "ust";
+            }
+
+            if (pbTop.Top >= pbSolR.Top && pbTop.Top<=pbSolR.Top+pbSolR.Height){
+                if (pbTop.Left==10){
+                    yatayYon = "sag";
+                }
+            }
+
+            if (pbTop.Top >= pbSagR.Top && pbTop.Top <= pbSagR.Top + pbSagR.Height)
+            {
+                if (pbTop.Left == 520)
+                {
+                    yatayYon = "sol";
+                }
+            }
+
+            if (pbTop.Left < 10)
+            {
+                solSkor++;
+                sayiOldu();
+                baslat();
+            }
+
+            if (pbTop.Left > 520)
+            {
+                sagSkor++;
+                sayiOldu();
+                baslat();
+            }
+
+          
+
+
+        }
+
+
+        public void sayiOldu(){
+            
+
+            topHiz = ((sagSkor+solSkor)/5+1)*10;
+            label3.Text = solSkor.ToString();
+            label4.Text = sagSkor.ToString();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baslat();
+        }
+
+        public void baslat()
+        {
+            pbTop.Location = new Point(240, 150);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Up)
+            {
+                if (pbSagR.Top >= 10)
+                {
+                    pbSagR.Top = pbSagR.Top - 10;
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (pbSagR.Top <= 260)
+                {
+                    pbSagR.Top = pbSagR.Top + 10;
+                }
+            }
+
+
+            if (e.KeyCode == Keys.A)
+            {
+                if (pbSolR.Top >= 10)
+                {
+                    pbSolR.Top = pbSolR.Top - 10;
+                }
+            }
+            if (e.KeyCode == Keys.Z)
+            {
+                if (pbSolR.Top <= 260)
+                {
+                    pbSolR.Top = pbSolR.Top + 10;
+                }
+            }
+        }
+    }
+}
